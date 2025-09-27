6a468e9c97463f694a2c65452c7969cb95d161c1
MejezZZ <jerahmeel6603@gmail.com>
2025-09-28 00:55:00 +0800

    update
    update sa code
    nawala yung code sa temp sensor

--- soil_moisture_sensor/soil_moisture_sensor.ino
+++ soil_moisture_sensor/soil_moisture_sensor.ino
@@ -2,6 +2,7 @@
 #include <DFRobotDFPlayerMini.h>
 #include <Wire.h>
 #include <SoftwareSerial.h>
+#include <DHT.h>
 
 // LCD
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -20,9 +21,15 @@ DFRobotDFPlayerMini myDFPlayer;
 #define wet 511
 #define dry 673
 
+// DHT11
+#define DHTPIN 2       // DHT11 data pin
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
 unsigned long prevMillis = 0;
-const long interval = 3000; // 3s message change
+const long interval = 3000; // 3s change
 bool wasThirsty = false;    // Track thirsty → watered state
+bool showTemp = false;      // Toggle between plant msg and temp
 
 void setup() {
   Serial.begin(9600);
@@ -34,6 +41,8 @@ void setup() {
   pinMode(redLED, OUTPUT);
   pinMode(greenLED, OUTPUT);
   pinMode(blueLED, OUTPUT);
+
+  dht.begin();
 
   if (!myDFPlayer.begin(mySoftwareSerial)) {
     lcd.setCursor(0, 0);
@@ -48,48 +57,69 @@ void loop() {
   int percent = map(value, wet, dry, 100, 0);
   percent = constrain(percent, 0, 100);
 
-  lcd.setCursor(0, 0);
-  lcd.print("Moisture: ");
-  lcd.print(percent);
-  lcd.print("%   ");
-
   if (millis() - prevMillis >= interval) {
     prevMillis = millis();
-    lcd.setCursor(0, 1);
-    lcd.print("                "); // clear line
+    lcd.clear();
 
-    if (percent <= 30) {
-      wasThirsty = true;
-      String msgs[] = {"I'm DYING", "Help meeee!", "So thirsty!"};
-      lcd.setCursor(0, 1);
-      lcd.print(msgs[random(0, 3)]);
-      myDFPlayer.play(random(1, 4)); // 0001–0003.mp3
-      flashRed();
-    } 
-    else if (percent <= 80) {
-      if (wasThirsty) {
-        lcd.setCursor(0, 1);
-        lcd.print("Yum, water! 💧");
-        myDFPlayer.play(4); // 0004.mp3
-        wasThirsty = false;
+    if (showTemp) {
+      float h = dht.readHumidity();
+      float t = dht.readTemperature();
+      if (isnan(h) || isnan(t)) {
+        lcd.setCursor(0, 0);
+        lcd.print("DHT11 Error!");
       } else {
-        String msgs[] = {"Yay", "Feeling fresh!", "So happy!"};
+        lcd.setCursor(0, 0);
+        lcd.print("Temp: ");
+        lcd.print(t);
+        lcd.print((char)223); // degree symbol
+        lcd.print("C");
+
+        lcd.setCursor(0, 1);
+        lcd.print("Humid: ");
+        lcd.print(h);
+        lcd.print("%");
+      }
+      showTemp = false;
+    } else {
+      lcd.setCursor(0, 0);
+      lcd.print("Moisture: ");
+      lcd.print(percent);
+      lcd.print("%   ");
+
+      if (percent <= 30) {
+        wasThirsty = true;
+        String msgs[] = {"I'm DYING", "Help meeee!", "So thirsty!"};
         lcd.setCursor(0, 1);
         lcd.print(msgs[random(0, 3)]);
+        myDFPlayer.play(random(0, 5)); // 0001–0004.mp3
+        flashRed();
+      } 
+      else if (percent <= 80) {
+        if (wasThirsty) {
+          lcd.setCursor(0, 1);
+          lcd.print("Yum, water!");
+          myDFPlayer.play(6); // 0005.mp3
+          wasThirsty = false;
+        } else {
+          String msgs[] = {"Yay", "Feeling fresh!", "So happy!"};
+          lcd.setCursor(0, 1);
+          lcd.print(msgs[random(0, 3)]);
+        }
+        greenLight();
+      } 
+      else {
+        lcd.setCursor(0, 1);
+        lcd.print("Zzz...");
+        blueBreathing();
       }
-      greenLight();
-    } 
-    else {
-      lcd.setCursor(0, 1);
-      lcd.print("Zzz...");
-      blueBreathing();
+      showTemp = true;
     }
   }
 }
 
 // LED patterns
 void flashRed() {
-  for (int i = 0; i < 5; i++) {   // flash 5 times
+  for (int i = 0; i < 5; i++) {
     digitalWrite(redLED, HIGH);
     delay(200);
     digitalWrite(redLED, LOW);
